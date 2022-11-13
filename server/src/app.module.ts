34dadd5b455b55f5325d9014f0cd355b6d3d57ac
June1010 <tjrwns1010@naver.com>
2022-11-14 06:42:14 +0900

    chore: Prisma, redis, swagger 연동

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { CacheModule, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { PrismaModule } from "./prisma/prisma.module";
+import * as redisStore from "cache-manager-ioredis";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        CacheModule.register({
+            isGlobal: true,
+            store: redisStore,
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+        }),
+        PrismaModule,
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, "..", "..", "client", "build"),
         }),

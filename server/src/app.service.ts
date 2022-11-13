109d55d045c8d35966e0c01a9160b7e51d134f0e
June1010 <tjrwns1010@naver.com>
2022-11-14 06:41:37 +0900

    chore: Prisma 초기 설정(엔터티 코드 작성 및 테스트)

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return "Hello World!";
+    constructor(private prismaService: PrismaService) {}
+
+    async doTest() {
+        return this.prismaService.user.create({
+            data: {
+                userId: "abcd1234",
+                salt: "1",
+                password: "1",
+                zipCode: 123,
+                pace: 123,
+            },
+        });
     }
 }

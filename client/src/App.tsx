a4351fca657149ae7021c96df30c2c9b7c6a2b77
catensia <catenarywkd1@gmail.com>
2022-11-14 11:46:08 +0900

    Bad commit : eslint and build fail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+    useEffect(() => {
+        const a = 1
+        const b = '123';
+    });
     return <div className="App">hello boostcamp?</div>;
 }
 

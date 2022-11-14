dcb861c14cd772a151adc7d5be38e363f8fc6413
catensia <catenarywkd1@gmail.com>
2022-11-14 11:55:01 +0900

    fix: pass eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
     useEffect(() => {
-        const a = 1
-        const b = '123';
+        const a = 1;
+        const b = "123";
     });
     return <div className="App">hello boostcamp?</div>;
 }

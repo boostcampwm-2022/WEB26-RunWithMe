3c66f5dee62e118bdc5bc20d0e4839ef8e09117c
choigeon96 <ck731@naver.com>
2022-11-16 22:09:31 +0900

    fix: #13 App.tsx 사용하지 않는 변수 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import useMap from "#hooks/useMap";
 
 function App() {
-    const { map, renderMap } = useMap({ height: "500px", center: { lat: 33.45, lng: 126.57 } });
+    const { renderMap } = useMap({ height: "500px", center: { lat: 33.45, lng: 126.57 } });
     return (
         <div className="App">
             hello boostcamp?

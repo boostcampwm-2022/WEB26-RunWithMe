7c5c4c976c408f8f8d7f34ca5184bc69143f2632
choigeon96 <ck731@naver.com>
2022-11-16 23:38:21 +0900

    fix: #13 ViewMap 코스 경로 받아오도록 수정

--- client/src/components/Map/ViewMap/ViewMap.tsx
+++ client/src/components/Map/ViewMap/ViewMap.tsx
@@ -1,11 +1,13 @@
 import useMap from "#hooks/useMap";
+import { LatLng } from "#types/LatLng";
 
 interface ViewMapProps {
     height?: string;
+    path?: LatLng[];
 }
 
 const ViewMap = ({ height = "50vh" }: ViewMapProps) => {
-    const { map, renderMap } = useMap({ height, center: { lat: 33.450701, lng: 126.570667 } });
+    const { renderMap } = useMap({ height, center: { lat: 33.450701, lng: 126.570667 } });
     return renderMap();
 };
 

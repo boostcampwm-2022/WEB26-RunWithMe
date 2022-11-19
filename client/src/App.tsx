1216027c1df76b8dde2ece5f5ba510c6ba8be697
choigeon96 <ck731@naver.com>
2022-11-20 01:05:31 +0900

    feat: #14 코스 등록 페이지 레이아웃

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import SignUp from "#pages/SignUp";
 import Login from "#pages/Login";
 import MainPage from "#pages/MainPage";
 import { Route, Routes } from "react-router-dom";
+import NewCourse from "#pages/NewCourse/NewCourse";
 
 function App() {
     return (
@@ -10,6 +11,9 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="signup" element={<SignUp />} />
             <Route path="login" element={<Login />} />
+            <Route path="course">
+                <Route path="new" element={<NewCourse />} />
+            </Route>
         </Routes>
     );
 }
---
+++ client/src/pages/NewCourse/NewCourse.tsx
@@ -0,0 +1,53 @@
+import Button from "#components/Button/Button";
+import Header from "#components/Header/Header";
+import Input from "#components/Input/Input";
+import { PLACEHOLDER } from "#constants/constants";
+import useMap from "#hooks/useMap";
+import styled from "styled-components";
+import { COLOR } from "styles/color";
+import { flexColumn } from "styles/flex";
+
+const CourseForm = styled.div`
+    ${flexColumn};
+    align-items: center;
+    width: 100%;
+    height: "240px";
+    box-shadow: 0px -4px 4px rgba(0, 0, 0, 0.25);
+    padding: 15px 27px;
+    div {
+        margin-bottom: 10px;
+        width: 100%;
+        p {
+            padding: 16px;
+            border-bottom: 1px solid ${COLOR.BABY_BLUE};
+        }
+    }
+`;
+
+const NewCourse = () => {
+    const { renderMap } = useMap({
+        height: `${window.innerHeight - 307}px`,
+        center: { lat: 33.450701, lng: 126.570667 },
+    });
+    return (
+        <div style={{ height: "100vh", maxHeight: "100vh" }}>
+            <Header text="코스 등록" loggedIn={true} />
+            {renderMap()}
+            <CourseForm>
+                <div>
+                    <span>총 거리</span>
+                    <p>3.3km</p>
+                </div>
+                <div>
+                    <span>코스명</span>
+                    <Input placeholder={PLACEHOLDER.COURSE_NAME}></Input>
+                </div>
+                <Button width="fit" onClick={console.log}>
+                    코스 등록
+                </Button>
+            </CourseForm>
+        </div>
+    );
+};
+
+export default NewCourse;

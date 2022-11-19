884cc212c54e9ee52950a77494231c46e14f2d60
catensia <catenarywkd1@gmail.com>
2022-11-19 14:25:03 +0900

    feat/loginpage : publish loginpage and refactor css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import SignUp from "#pages/SignUp";
+import Login from "#pages/Login";
+import MainPage from "#pages/MainPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return <SignUp />;
+    return (
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<Login />} />
+        </Routes>
+    );
 }
 
 export default App;

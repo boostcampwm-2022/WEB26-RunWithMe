3f1feaa98bccc13af6a9b64f63fcd4a1a4a823ad
June1010 <tjrwns1010@naver.com>
2022-11-22 14:34:25 +0900

    feat: 유저 엔티티 추가

--- server/src/entities/user_recruit.entity.ts
+++ server/src/entities/user_recruit.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 import { Recruit } from "./recruit.entity";
 
@@ -8,8 +8,23 @@ export class UserRecruit {
     id: number;
 
     @ManyToOne(() => User, (user) => user.userRecruits)
+    @JoinColumn({ name: "userId", referencedColumnName: "id" })
     user: User;
 
+    @Column()
+    userId: number;
+
     @ManyToOne(() => Recruit, (recruit) => recruit.userRecruits)
+    @JoinColumn({ name: "recruitId", referencedColumnName: "id" })
     recruit: Recruit;
+
+    @Column()
+    recruitId: number;
+
+    static of(userId: number, recruitId: number) {
+        const userRecruit = new UserRecruit();
+        userRecruit.userId = userId;
+        userRecruit.recruitId = recruitId;
+        return userRecruit;
+    }
 }

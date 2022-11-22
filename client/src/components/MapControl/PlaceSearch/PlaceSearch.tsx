d2de6bcb99a45e18aac7c74958f8f152112313a6
choigeon96 <ck731@naver.com>
2022-11-22 21:22:43 +0900

    feat: #62 PlaceSearch style 코드 분리

--- client/src/components/MapControl/PlaceSearch/PlaceSearch.tsx
+++ client/src/components/MapControl/PlaceSearch/PlaceSearch.tsx
@@ -1,64 +1,10 @@
 import { SEARCH_ICON } from "#assets/icons";
 import usePlaceSearch from "#hooks/usePlaceSearch";
-import { LocalData } from "#types/Local";
-import { MapControlPotition, MapControlProps } from "#types/MapControlProps";
+import { MapControlProps } from "#types/MapControlProps";
 import { PlaceInfo } from "#types/Place";
 import { debounce } from "#utils/timerUtils";
-import { ChangeEvent, useCallback, useRef, useState } from "react";
-import styled, { css } from "styled-components";
-import { COLOR } from "styles/color";
-import { flexColumn, flexRowCenter, flexRowSpaceBetween } from "styles/flex";
-import { fontMidium } from "styles/font";
-
-const SearchBarItemStyle = css`
-    background: ${COLOR.WHITE};
-    opacity: 0.9;
-    border-radius: 10px;
-    padding: 4px 8px;
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-`;
-
-const SearchContainer = styled.div<{ position: MapControlPotition }>`
-    ${flexColumn}
-    align-items: center;
-    position: absolute;
-    width: 100%;
-    z-index: 1;
-    top: ${({ position }) => position.top ?? "auto"};
-    left: ${({ position }) => position.left ?? "auto"};
-    bottom: ${({ position }) => position.bottom ?? "auto"};
-    right: ${({ position }) => position.right ?? "auto"};
-`;
-
-const SearchWrapper = styled.div`
-    ${flexRowCenter};
-    width: 100%;
-    div {
-        ${flexRowSpaceBetween};
-        ${SearchBarItemStyle};
-    }
-    input {
-        ${fontMidium(COLOR.DARK_GRAY, 500)}
-        border: none;
-        :focus {
-            outline: none;
-        }
-    }
-    margin-bottom: 4px;
-`;
-
-const PlaceList = styled.ul`
-    ${SearchBarItemStyle};
-    background-color: ${COLOR.WHITE};
-    width: 200px;
-    align-items: center;
-    li {
-        ${fontMidium(COLOR.DARK_GRAY, 500)};
-        margin: 2px 0;
-        list-style-type: none;
-    }
-`;
-
+import { ChangeEvent, useCallback, useRef } from "react";
+import { PlaceList, SearchContainer, SearchWrapper } from "./PlaceSearch.styles";
 interface PlaceSearchProps extends MapControlProps {
     setCenter: (position: kakao.maps.LatLng) => void;
     getCenter: () => kakao.maps.LatLng;

bb52843b9a1ba496938777ae18c5064c65b519e1
choigeon96 <ck731@naver.com>
2022-11-24 19:21:06 +0900

    feat: #78 코스 등록 API 작성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -14,6 +14,7 @@ import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
 import type { ClientOpts } from "redis";
 import { RecruitModule } from "./recruit/recruit.module";
+import { CourseModule } from "./course/course.module";
 
 @Module({
     imports: [
@@ -44,6 +45,7 @@ import { RecruitModule } from "./recruit/recruit.module";
         UserModule,
         AuthModule,
         RecruitModule,
+        CourseModule,
     ],
     controllers: [AppController],
     providers: [AppService],

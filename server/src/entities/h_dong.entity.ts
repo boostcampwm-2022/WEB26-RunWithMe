6cc2069e229b381abadd2295fc8b49fd04ddbc4c
June1010 <tjrwns1010@naver.com>
2022-11-24 15:53:20 +0900

    fix: synchronize option true로 인한 PK 중복 문제 임시 해결

--- server/src/entities/h_dong.entity.ts
+++ server/src/entities/h_dong.entity.ts
@@ -1,8 +1,11 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("h_dong")
 export class HDong {
-    @PrimaryColumn()
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
     code: string;
 
     @Column()

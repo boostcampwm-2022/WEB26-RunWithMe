18c182f158959d0179e0745ee555d63d149ac3fa
choigeon96 <ck731@naver.com>
2022-11-30 19:00:02 +0900

    fix: clear build error

--- client/src/hooks/queries/useRefreshQuery.ts
+++ client/src/hooks/queries/useRefreshQuery.ts
@@ -1,15 +1,18 @@
 import { userState } from "#atoms/userState";
 import { TIME } from "#constants/time";
 import useHttpGet from "#hooks/http/useHttpGet";
-import AuthRefreshRes from "#types/dto/AuthRefreshRes";
+import HttpResponse from "#types/dto/HttpResponse";
+import UserInfo from "#types/UserInfo";
 import { useQuery } from "@tanstack/react-query";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 
 const useRefreshQuery = () => {
-    const setUserInfo = useSetRecoilState(userState);
-    const { get } = useHttpGet<AuthRefreshRes>();
-    return useQuery(["refresh"], async () => get("/auth/refresh").then((res) => res || {}), {
-        onSuccess: (response) => setUserInfo(response),
+    const [userInfo, setUserInfo] = useRecoilState(userState);
+    const { get } = useHttpGet<HttpResponse<UserInfo>>();
+
+    return useQuery(["refresh"], async () => get("/auth/refresh").then((res) => res.data || {}), {
+        onSuccess: setUserInfo,
+        retry: !!(userInfo && userInfo.accessToken),
         refetchInterval: TIME.ACCESS_TOKEN_EXPIRE_TIME - TIME.MINUTE_IN_SECONDS,
     });
 };

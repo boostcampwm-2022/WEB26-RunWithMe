8f329d9425e9b7947cef10bc0484a3c245453298
pushedrumex <pushedrumex@naver.com>
2022-11-30 17:55:09 +0900

    hotfix: solve accessToken parsing issue

--- server/src/common/interceptors/http-request/http-request-body.interceptor.ts
+++ server/src/common/interceptors/http-request/http-request-body.interceptor.ts
@@ -1,4 +1,4 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor, RequestTimeoutException } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { CustomJwtService } from "src/common/modules/custom-jwt/custom-jwt.service";
 
@@ -8,15 +8,15 @@ export class HttpRequestBodyInterceptor implements NestInterceptor {
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const request = context.switchToHttp().getRequest();
-        const accessToken = request.headers["authorization"];
+        let accessToken = request.headers["authorization"];
 
         if (accessToken) {
             try {
+                accessToken = accessToken.split("Bearer")[1].trim();
                 const { userIdx } = this.jwtService.verifyAccessToken(accessToken);
                 if (request.method === "GET") {
                     request.params.userId = userIdx;
                 }
-
                 if (request.method === "POST") {
                     request.body.userId = userIdx;
                 }

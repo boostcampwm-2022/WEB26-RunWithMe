e2a6936984d3d8bfc86d277bfafeae1876b2acdc
choigeon96 <ck731@naver.com>
2022-12-07 15:16:42 +0900

    refactor: #203 webpack -> vite

--- client/src/hooks/http/useAxios.ts
+++ client/src/hooks/http/useAxios.ts
@@ -8,7 +8,7 @@ const useAxios = () => {
     const { current: axios } = useRef(
         Axios.create({
             withCredentials: true,
-            baseURL: process.env.REACT_APP_API_URL || "",
+            baseURL: import.meta.env.VITE_API_URL || "",
         }),
     );
 

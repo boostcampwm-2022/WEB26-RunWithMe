06fd771c1f1fe4e2db73c167db0bb118d15981b2
pushedrumex <pushedrumex@naver.com>
2022-12-07 19:33:50 +0900

    remove untracked file

--- test.js
+++
@@ -1,8 +0,0 @@
-console.log(new Date());
-const nowUtc = Date.now();
-const timeOff = new Date().getTimezoneOffset() * 60000;
-const today = new Date(nowUtc - timeOff).toISOString().substring(0, 16);
-console.log(today);
-const possibleTime = new Date(nowUtc - timeOff + 1000 * 60 * 60)
-  .toISOString()
-  .substring(0, 16);

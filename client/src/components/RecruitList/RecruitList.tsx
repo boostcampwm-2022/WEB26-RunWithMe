93ba350b4ebbd1050715fb8339cf099f59050c40
choigeon96 <ck731@naver.com>
2022-12-09 03:37:38 +0900

    refactor: recruit 목록 페이지 useInfiniteQuery로 전환

---
+++ client/src/components/RecruitList/RecruitList.tsx
@@ -0,0 +1,35 @@
+import RecruitCard from "#components/Card/RecruitCard/RecruitCard";
+import useRecruitListQuery from "#hooks/queries/useRecruitListQuery";
+import InfiniteScroll from "react-infinite-scroller";
+import styled from "styled-components";
+const CardWrapper = styled.div`
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+`;
+
+export interface RecruitListProps {
+    distance?: { min: number; max: number };
+    time?: { min: number; max: number };
+    query?: string;
+    authorFilter?: boolean;
+    titleFilter?: boolean;
+    availFilter?: boolean;
+}
+
+const RecruitList = (props: RecruitListProps) => {
+    const { data, fetchNextPage, hasNextPage } = useRecruitListQuery({ ...props });
+
+    return (
+        <InfiniteScroll loadMore={() => fetchNextPage()} hasMore={hasNextPage} loader={<h4>Loading...</h4>}>
+            <CardWrapper>
+                {data?.pages.map((page, pageIdx) =>
+                    page.map((card, idx) => <RecruitCard data={card} key={`${pageIdx}_${idx}`} />),
+                )}
+            </CardWrapper>
+        </InfiniteScroll>
+    );
+};
+
+export default RecruitList;
---
+++ client/src/hooks/queries/useRecruitListQuery.ts
@@ -0,0 +1,36 @@
+import { RecruitListProps } from "#components/RecruitList/RecruitList";
+import useHttpGet from "#hooks/http/useHttpGet";
+import HttpResponse from "#types/dto/HttpResponse";
+import { RecruitFilterParams } from "#types/FilterParams";
+import { Recruit } from "#types/Recruit";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const useRecruitListQuery = ({ distance, query, availFilter, authorFilter, titleFilter, time }: RecruitListProps) => {
+    const { get } = useHttpGet<HttpResponse<Recruit[]>>();
+
+    const recruitQueryParams = (page: number): RecruitFilterParams => {
+        const param: any = {};
+        param.title = titleFilter ? "true" : "false";
+        param.author = authorFilter ? "true" : "false";
+        param.avail = availFilter ? "true" : "false";
+        if (query !== "") param.query = query;
+        if (distance) {
+            param.maxLen = (distance.max * 1000).toString();
+            param.minLen = (distance.min * 1000).toString();
+        }
+        if (time) {
+            param.time = time.max.toString();
+        }
+        param.page = page;
+        return param;
+    };
+
+    return useInfiniteQuery(
+        ["recruits", distance?.min, distance?.max, authorFilter, titleFilter, query],
+        ({ pageParam = 1 }) => get("/recruit", recruitQueryParams(pageParam)).then((res) => res.data),
+        {
+            getNextPageParam: (lastPage, allPages) => lastPage.length > 0 && allPages.length + 1,
+        },
+    );
+};
+export default useRecruitListQuery;

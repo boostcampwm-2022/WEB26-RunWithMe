86f9ba95a5d50517472b5dedbee2a8fc74188dc7
choigeon96 <ck731@naver.com>
2022-12-13 13:17:16 +0900

    refactor: 지도 잠금 버튼 아이콘 변경

--- client/src/components/MapControl/LockButton/LockButton.tsx
+++ client/src/components/MapControl/LockButton/LockButton.tsx
@@ -1,4 +1,4 @@
-import { LOCK_ICON, UNLOCK_ICON } from "#assets/icons";
+import { STRAIGHT_ICON, CURVE_ICON } from "#assets/icons";
 import { MapControlProps } from "#types/MapControlProps";
 import { MapButton } from "../MapControl.styles";
 interface LockButtonProps extends MapControlProps {
@@ -8,7 +8,7 @@ interface LockButtonProps extends MapControlProps {
 const LockButton = ({ isLocked, onClick, position = { top: "96px", right: "14px" } }: LockButtonProps) => {
     return (
         <MapButton onClick={onClick} position={position}>
-            <img width="14" height="14" alt="LOCK_ICON" src={isLocked ? LOCK_ICON : UNLOCK_ICON} />
+            <img width="14" height="14" alt="LOCK_ICON" src={isLocked ? CURVE_ICON : STRAIGHT_ICON} />
         </MapButton>
     );
 };
